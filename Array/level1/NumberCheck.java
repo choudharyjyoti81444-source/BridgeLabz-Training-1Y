5985e89e58dfe6e9abdca762bf674ad862f39107
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-02-11 21:31:11 +0530

    Added:My Array Assignment

---
+++ Array/level1/NumberCheck.java
@@ -0,0 +1,47 @@
+package com.gla.Array.level1;
+
+import java.util.Scanner;
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // a. Define an integer array of 5 elements
+        int[] arr = new int[5];
+
+        // Get user input
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // b & c. Loop through the array and check conditions
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is Positive and Even");
+                } else {
+                    System.out.println(num + " is Positive and Odd");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is Negative");
+            } else {
+                System.out.println(num + " is Zero");
+            }
+        }
+
+        // d. Compare first and last elements
+        int first = arr[0];
+        int last = arr[arr.length - 1];
+
+        if (first == last) {
+            System.out.println("First and last elements are Equal");
+        } else if (first > last) {
+            System.out.println("First element is Greater than last element");
+        } else {
+            System.out.println("First element is Less than last element");
+        }
+    }
+}
+
