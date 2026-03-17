cbb1953f9421e0f723c9b70bba9ac79e84e87849
choudharyjyoti81444 <choudharyjyoti81444@gmail.com>
2026-03-17 18:16:28 +0530

    Added:My Class Practice

---
+++ Class-Practice/HybridExample.java
@@ -0,0 +1,27 @@
+import java.util.*;
+interface A {
+    void showA();}
+
+interface B {
+    void showB(); }
+
+    class C implements A , B {
+        public void showA(){
+        System.out.println("Method of A ");}
+	
+        public void showB(){
+        System.out.println("Method of B");}
+    }
+
+    class D extends C {
+        void showD(){
+        System.out.println("method of D");}
+    }
+	
+public class ṣ {	
+    public static void main (String[] args){
+    obj.showA();
+    obj.showB();
+    obj.showD();
+    }
+}
